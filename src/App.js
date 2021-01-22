7505c3c69cc816b1e4c963d7699acf8a217b502e
Pedro Cardozo <p-cardozo@hotmail.com>
2021-01-22 19:39:09 -0300

    Adding ESlint and Parcel

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
-    
     React.createElement("h1", {}, "Adopt Me!"),
-    
+
     React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
     }),
 
     React.createElement(Pet, {
-        name: "Pepper",
-        animal: "Bird",
-        breed: "HShae",
+      name: "Pepper",
+      animal: "Bird",
+      breed: "HShae",
     }),
-
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
