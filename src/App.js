837d9bfe064e4621cdede100b2e14a3fbc59c7a7
Pedro Cardozo <p-cardozo@hotmail.com>
2021-01-25 20:34:26 -0300

    Adding Hooks and JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParms from "./SearchParms";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "HShae",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>AdoptMe</h1>
+      <SearchParms />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
