298183b049dd2e9f80534897ddc4752dac3c5336
Pedro Cardozo <p-cardozo@hotmail.com>
2021-01-27 20:18:49 -0300

    Nothing to say about it XD

--- src/SearchParms.js
+++ src/SearchParms.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
+import useDropdown from "./useDropdown";
 
 const SearchParms = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animal } = pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animal || []);
+  }
+
+  useEffect(() => {
+    setBreed([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedsString = breeds.map(({ name }) => name);
+      setBreeds(breedsString);
+    }, console.error);
+  }, [animal]);
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+
         <button>Submit</button>
       </form>
+      <Results pets={pets} />;
     </div>
   );
 };
